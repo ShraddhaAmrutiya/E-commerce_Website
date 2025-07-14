d8a353042e6a996b0729efe996ba897c63a7bf7e
Shraddha <shraddha.amrutiya@vindaloosofttech.com>
2025-07-14 18:22:31 +0530

    .

--- dist/Controllers/reviewController.js
+++ dist/Controllers/reviewController.js
@@ -6,39 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getReviews = exports.addReview = void 0;
 const reviewModel_1 = __importDefault(require("../Models/reviewModel"));
 const productModel_1 = require("../Models/productModel");
-// const addReview = async (req, res) => {
-//     try {
-//         const { rating, comment } = req.body;
-//         const { id: productId } = req.params;
-//         const userId = req.user?.id;
-//         if (!userId) {
-//             return res.status(401).json({ message: req.t("review.unauthorized") });
-//         }
-//         const product = await productModel_1.Product.findById(productId);
-//         if (!product) {
-//             return res.status(404).json({ message: req.t("review.productNotFound") });
-//         }
-//         const existingReview = await reviewModel_1.default.findOne({ product: productId, user: userId });
-//         if (existingReview) {
-//             return res.status(400).json({ message: req.t("review.alreadyReviewed") });
-//         }
-//         const review = new reviewModel_1.default({
-//             product: productId,
-//             user: userId,
-//             rating,
-//             comment,
-//         });
-//         await review.save();
-//         const reviews = await reviewModel_1.default.find({ product: productId }).populate("user", "userName");
-//         const averageRating = reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
-//         product.rating = parseFloat(averageRating.toFixed(1));
-//         await product.save();
-//         res.status(201).json({ message: req.t("review.reviewSuccess"), review });
-//     }
-//     catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
 
 const addReview = async (req, res) => {
   try {
