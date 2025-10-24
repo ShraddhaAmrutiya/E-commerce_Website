509b7ff91fb46d9c390570e28a07b476dcff1657
Shraddha <shraddhaamrutiya@gmail.com>
2025-10-24 22:38:40 +0530

    add origine

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ import { google } from "googleapis";
 import { User } from "./Models/userModel";
 
 dotenv.config();
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000","https://spectacular-mousse-8d8a6f.netlify.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000","https://aaraksha-resin-art.netlify.app"];
 
 const app = express();
 app.use(
